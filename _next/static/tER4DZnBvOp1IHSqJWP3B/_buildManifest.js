2127de6835dd5a2fa259d797508f06a660b0464e
ritujoshi-cmyk <ritujoshi-cmyk@users.noreply.github.com>
2026-07-07 09:14:50 +0000

    deploy: dba7589e044778992c6bdb3551a93c8aedb8e5d1

---
+++ _next/static/tER4DZnBvOp1IHSqJWP3B/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Office-Premises-Cost-analysis//_next/:path+",
+        "destination": "/Office-Premises-Cost-analysis/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
